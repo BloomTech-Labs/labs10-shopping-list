b1120eb59fb8df6989172f314f229ecc01930c36
Kevin Nguyen <kevinn858@gmail.com>
2019-02-14 10:54:21 -0800

    added columns to categories table

--- backend/data/dev/migrations/20190214104844_categories_table.js
+++ backend/data/dev/migrations/20190214104844_categories_table.js
@@ -1,8 +1,15 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('categories', (table) => {
+      //have an unique ID for each category (done with increments)
+      table.increments('id');
+
+      // category name, will be constrained with uniqueness
+      // since there is no point in having separate categories of the same thing
+      table.string('category', 255).notNullable().unique();
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('categories');
 };
