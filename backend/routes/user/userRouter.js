543fa5873a70f316e2a8ae09ee4794c868d286cd
iAmAdamReid <adam@adamreid.me>
2019-02-14 14:46:08 -0800

    Can add user to database with Postman

--- backend/routes/user/userRouter.js
+++ backend/routes/user/userRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const userRouter = express.Router();
-
+const userDb = require('../../helpers/userModel');
 const checkJwt = require('../../validators/checkJwt');
 
 // userRouter.use(secured);
@@ -11,5 +11,20 @@ userRouter.get('/', checkJwt, (req, res) => {
     console.log(req.body);
     res.send('This is the /user root endpoint.');
 });
+
+userRouter.post('/', (req, res) => {
+    console.log(req.body);
+
+    let user = req.body;
+
+    userDb.add(user).then(id => {
+        console.log(id);
+        return res.status(200).json({message: `User adder to database with ID ${id}`, id: id});
+    })
+    .catch(err => {
+        console.log(err);
+        return res.status(500).json({error: `Error when adding user.`});
+    })
+})
 
 module.exports = userRouter;
