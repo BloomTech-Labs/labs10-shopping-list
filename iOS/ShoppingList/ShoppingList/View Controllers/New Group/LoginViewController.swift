25dc27e3429ba4e10b342c559cae9fb3c543340e
Jason Modisett <jason@Jasons-MacBook-Pro.local>
2019-02-14 16:24:05 -0600

    Added barebones login UI + isLoggedIn check

---
+++ iOS/ShoppingList/ShoppingList/View Controllers/New Group/LoginViewController.swift
@@ -0,0 +1,25 @@
+//
+//  LoginViewController.swift
+//  ShoppingList
+//
+//  Created by Jason Modisett on 2/14/19.
+//  Copyright © 2019 Lambda School Labs. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController, StoryboardInstantiatable {
+    
+    static let storyboardName: StoryboardName = "LoginViewController"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func loginButtonPressed(_ sender: Any) {
+        defaults.set(true, forKey: Keys.isUserLoggedInKey)
+        UIApplication.shared.keyWindow?.rootViewController = MainViewController.instantiate()
+    }
+
+}
