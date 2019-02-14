4d02e96b085d85a32c241ac7c5e061585675eaa2
Jason Modisett <jason@Jasons-MacBook-Pro.local>
2019-02-14 10:09:10 -0600

    Added frameworks, MainViewController + storyboard, initial UI

---
+++ iOS/ShoppingList/ShoppingList/Frameworks/Chameleon/ShoppingList-Bridging-Header.h
@@ -0,0 +1,8 @@
+// Copyright © 2019 Jason Modisett. All rights reserved.
+
+#ifndef ShoppingList_Bridging_Header_h
+#define ShoppingList_Bridging_Header_h
+
+#import "Chameleon.h"
+
+#endif /* ShoppingList_Bridging_Header_h */
---
+++ iOS/ShoppingList/ShoppingList/MainViewController/MainViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MainViewController.swift
+//  Shopping List
+//
+//  Created by Jason Modisett on 2/14/19.
+//  Copyright © 2019 Jason Modisett. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController, StoryboardInstantiatable {
+
+    static let storyboardName: StoryboardName = "MainViewController"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
