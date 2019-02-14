e7e80957eac634ee0f60accb0eb0f64d7de3f61c
iAmAdamReid <adam@adamreid.me>
2019-02-14 01:57:23 -0800

    PassportJS auth0 permits google and github login strategies, still figuring out token storage and protection middleware

--- backend/routes/auth/authRouter.js
+++ backend/routes/auth/authRouter.js
@@ -24,27 +24,30 @@ if(process.env.NODE_ENV === 'production'){
 }
 
 
+authRouter.use(passport.initialize());
+authRouter.use(passport.session());
 authRouter.use(session(sess));
-
 
 const auth0_strategy = new Auth0Strategy({
     domain: process.env.AUTH0_DOMAIN,
     clientID: process.env.AUTH0_CLIENT_ID,
     clientSecret: process.env.AUTH0_CLIENT_SECRET,
     callbackURL:
-        process.env.AUTH0_CALLBACK_URL || 'https://localhost:9000/api/auth/callback'
+        process.env.AUTH0_CALLBACK_URL || 'http://localhost:9000/api/auth/callback'
 },
-    function(accessToken, refreshToken, extraParams, profile, done){
+    async function(accessToken, refreshToken, extraParams, profile, done){
         // accessToken is the token to call Auth0 API (not needed in the most cases)
         // extraParams.id_token has the JSON Web Token
         // profile has all the information from the user
+        console.log(accessToken);
+        console.log('token', extraParams.id_token);
+        console.log('profile', profile);
         return done(null, profile);
     }
 );
 
 passport.use(auth0_strategy);
-authRouter.use(passport.initialize());
-authRouter.use(passport.session());
+
 
 authRouter.get('/', (req, res) => {
     res.send('This is the /auth root endpoint.');
@@ -57,25 +60,50 @@ authRouter.get('/success', (req, res) => {
 
 authRouter.get('/login',
     passport.authenticate('auth0', {
-        scope: 'openid email profile'
+        scope: 'openid email profile',
+
+        // connection: 'google-oauth2'
     }), function(req, res){
-        res.redirect('https://localhost:9000/api/auth/success');
+        res.redirect('http://localhost:9000/api/auth/success');
     });
+
+authRouter.get('/login/google',
+    passport.authenticate('auth0', {connection: 'google-oauth2'}), function(req, res){
+        res.redirect('http://localhost:9000/api/auth/success');
+    })
 
 
 authRouter.get('/callback', function(req, res, next){
     passport.authenticate('auth0', function(err, user, info){
+        console.log(req.user);
         if(err){ return next(err) }
-        if(!user){ return res.redirect('https://localhost:9000/api/auth/login'); }
+        if(!user){ return res.redirect('http://localhost:9000/api/auth/login'); }
         req.logIn(user, function(err){
             if(err){ return next(err); }
             const returnTo = req.session.returnTo;
             delete req.session.returnTo;
-            res.redirect(returnTo || 'https://localhost:9000/api/auth/success');
+            res.redirect(returnTo || 'http://localhost:9000/api/auth/success');
         });
     })(req, res, next);
 });
 
 
+
+passport.serializeUser(function(user, done){
+    console.log('serialize', user);
+    done(null, user);
+});
+
+passport.deserializeUser(function(user, done){
+    console.log('deserialize', user)
+    done(null, user);
+})
+
+
+authRouter.get('/logout', (req, res) => {
+    console.log(req.user);
+    req.logout();
+    res.redirect('http://localhost:9000');
+})
 
 module.exports = authRouter;
