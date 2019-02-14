de6cb610cab280ec1f699614a8bf9811f0fd8877
iAmAdamReid <adam@adamreid.me>
2019-02-14 14:10:43 -0800

    remove default styling, add contrast

--- frontend/shopping-list/src/App.js
+++ frontend/shopping-list/src/App.js
@@ -6,10 +6,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-          <h1>Shopping List</h1>
-        <div className='listHeader'>
-          <h2>Lamont House</h2>
-        </div>
         <TestView />
       </div>
     );
