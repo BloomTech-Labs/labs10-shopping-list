91a835b4e86438333604341cc25ca6bf37f9ee97
iAmAdamReid <adam@adamreid.me>
2019-02-15 08:08:43 -0800

    can get users in group with group ID at GET /api/group/:id/users

--- backend/routes/group/groupRouter.js
+++ backend/routes/group/groupRouter.js
@@ -2,10 +2,9 @@ const express = require('express');
 const groupRouter = express.Router();
 
 const groupDb = require('../../helpers/groupModel');
+const groupMembersDb = require('../../helpers/groupMembersModel');
 
 const checkJwt = require('../../validators/checkJwt');
-
-
 
 /**
  * Check Group Size middleware
@@ -14,11 +13,7 @@ const checkJwt = require('../../validators/checkJwt');
  */
 
 
-/** @TODO Update group schema to designate a group "owner"
- * This will be important when deciding the maximum limit of
- * users in the group. The group must be owned by a premium user
- * in order to hold more than 2 members
- */
+
 
 
 /****************************************************************************************************/
@@ -48,10 +43,39 @@ groupRouter.get('/', (req, res) => {
  * Get Group by ID
  */
 
+groupRouter.get('/:id', (req, res) => {
+    groupDb.get().then(group => {
+        if(!group){
+            return res.status(404).json({error: `Could not find group with ID ${id}.`})
+        } else {
+            return res.status(200).json(group);
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        return res.status(500).json({error: `Error retrieving group.`})
+    })
+})
+
 
 /**
- * Get all users in group
+ * Get all users in group by Group ID
  */
+groupRouter.get('/:id/users', (req, res) => {
+    const id = req.params.id;
+    groupMembersDb.get(id).then(members => {
+        if(!members){
+            return res.status(404).json({error: `Could not find members in group with ID ${id}.`})
+        } else {
+            return res.status(200).json(group);
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        return res.status(500).json({error: `Error retrieving group members.`})
+    })
+})
+
 
 /**
  * Create new group
@@ -61,10 +85,13 @@ groupRouter.get('/', (req, res) => {
  * Add member to group
  */
 
-/**
+/**n
  * Remove member from group
  */
 
 /**
  * Update Group Name
+ */
+
+ /** Designate group moderator (true/false)
  */
