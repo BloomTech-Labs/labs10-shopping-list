1ffadb6ac59eff57286edb1b1b7a4de877da7a47
iAmAdamReid <adam@adamreid.me>
2019-02-19 09:23:50 -0800

    build out auth0 WebAuth class

--- frontend/shopping-list/src/App.js
+++ frontend/shopping-list/src/App.js
@@ -2,7 +2,10 @@ import React, { Component } from 'react';
 import './App.css';
 import TestView from './views/testView.js';
 import LandingPage from './views/landingPageView.js';
+import Login from './components/Login';
+
 import {Route} from 'react-router-dom';
+
 
 class App extends Component {
   render() {
@@ -10,6 +13,8 @@ class App extends Component {
       <div className="App">
         <Route exact path='/' component={LandingPage} />
         <Route path='/test' component={TestView} />
+        <Route path = '/login' component = {Login} />
+
       </div>
     );
   }
---
+++ frontend/shopping-list/src/components/Callback.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+
+class Callback extends React.Component {
+    constructor(){
+        super();
+        this.state = {};
+    }
+    
+    render(){
+        return(
+            <div className = 'callback-container'>
+            
+            </div>
+        )
+    }
+}
+
+export default Callback;
