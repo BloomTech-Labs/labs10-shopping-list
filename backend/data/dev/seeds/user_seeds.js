c849372096a7a4960e704e8174b74421a514ed98
Jeremy Boggs <nicocchilover@gmail.com>
2019-02-20 12:40:39 -0500

    updated user seeds for testing notifs

--- backend/data/dev/seeds/user_seeds.js
+++ backend/data/dev/seeds/user_seeds.js
@@ -5,12 +5,17 @@ const createFakeUser = () => ({
   email: faker.internet.email()
 });
 
-exports.seed = async function(knex, Promise) {
+exports.seed = function(knex, promise) {
   let fakeUsers = [];
   const desiredFakeUsers = 500;
   for (let i = 0; i < desiredFakeUsers; i++) {
     fakeUsers.push(createFakeUser());
   }
 
-  await knex("users").insert(fakeUsers);
-}
+  // Deletes ALL existing entries and resets primary keys
+  return knex(`users`).truncate()
+      .then(function () {
+        // Inserts seed entries
+        return knex(`users`).insert(fakeUsers);
+      });
+};
