6084d4dd54b165e5dd4551333143b732d9e31af2
Kevin Nguyen <kevinn858@gmail.com>
2019-02-20 11:31:56 -0800

    cleaned up warnings

--- frontend/shopping-list/src/components/Navigation.js
+++ frontend/shopping-list/src/components/Navigation.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Link, withRouter} from 'react-router-dom';
+import {withRouter} from 'react-router-dom';
+// import {Link, withRouter} from 'react-router-dom';
 import auth0Client from './Auth';
 
 class Navigation extends React.Component{
