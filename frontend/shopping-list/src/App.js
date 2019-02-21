7c4b0f2c8a2032e4f6e97a604add7b824e520af3
Jeremy Boggs <nicocchilover@gmail.com>
2019-02-20 19:12:07 -0500

    fixed typos

--- frontend/shopping-list/src/App.js
+++ frontend/shopping-list/src/App.js
@@ -4,11 +4,7 @@ import {Route} from 'react-router-dom';
 import Home from './components/Home';
 import UserProfile from './components/UserProfile';
 import Callback from './components/Callback';
-<<<<<<< HEAD
 import GroupsProfile from "./components/GroupsProfile";
-=======
-import GroupsProfile from './components/GroupsProfile';
->>>>>>> 8d69328970f921cb32680232591cf04a981947e5
 
 class App extends Component {
   render() {
