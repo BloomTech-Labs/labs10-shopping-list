fcd1c1c9fc64e10dcf7c7edd70bd3aa4993b0cd1
Jeremy Boggs <nicocchilover@gmail.com>
2019-02-21 10:27:16 -0500

    navigation styles update

--- frontend/shopping-list/src/components/Navigation.js
+++ frontend/shopping-list/src/components/Navigation.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {withRouter} from 'react-router-dom';
 // import {Link, withRouter} from 'react-router-dom';
 import auth0Client from './Auth';
 import './Styles/Navigation.css';
+import { MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBNavbarToggler, MDBCollapse, MDBDropdown,
+    MDBDropdownToggle, MDBDropdownMenu, MDBDropdownItem, MDBIcon, MDBFormInline, MDBBtn } from "mdbreact";
 
-class Navigation extends React.Component{
+class Navigation extends Component{
+    state = {
+        collapseID: "",
+        activeTabClassname: "home"
+    };
+
+    toggleCollapse = collapseID => () =>
+        this.setState(prevState => ({
+            collapseID: prevState.collapseID !== collapseID ? collapseID : ""
+        }));
     
     signOut = () => { // logs out the current user and redirects to the homepage
         auth0Client.signOut();
@@ -20,31 +31,54 @@ class Navigation extends React.Component{
     }
 
     render(){
+        const id = localStorage.getItem("userId");
+        let isLogged = false;
+        if (id !== null) isLogged = true;
+        const pathname = this.props.location.pathname;
         return(
-            <div className="Navbar">
-                {
-                    !localStorage.getItem('email') && 
-                    <div className='button' onClick={auth0Client.signIn}>Sign In</div>
-                }
+            <MDBNavbar color="default-color" dark expand="md">
+                <MDBNavbarBrand>
+                    <strong className="white-text">ShopTrak</strong>
+                </MDBNavbarBrand>
+                <MDBNavbarToggler onClick={this.toggleCollapse} />
+                <MDBCollapse id="navbarCollapse3" isOpen={this.state.isOpen} navbar>
+                    <MDBNavbarNav left>
+                        <MDBNavItem active={pathname === "/" ? "active" : null} >
+                            <MDBNavLink to="/">Home</MDBNavLink>
+                        </MDBNavItem>
+                        {isLogged ? (
+                            <MDBNavItem active={pathname === "/groups" ? "active" : null} >
+                                <MDBNavLink to="/groups">Groups</MDBNavLink>
+                            </MDBNavItem>
+                        ) : null}
 
-                {
-                    localStorage.getItem('email') && 
-                    <div className='signedInNavBar'>
-                        <div className='userGreeting'>
-                            Hello, {localStorage.getItem('name')}
-                        </div>
-                        <div className='buttons'>
-                            <div className='button' onClick={this.goToHome}>
-                                Home
-                            </div>
-                            <div className='button' onClick={this.goToProfile}>
-                                Profile
-                            </div>
-                            <div className='button' onClick = {this.signOut}>Sign out</div>
-                        </div>
-                    </div>
-                }
-            </div>
+                    </MDBNavbarNav>
+                    <MDBNavbarNav right>
+                        <MDBNavItem>
+                            {isLogged ? (
+                                <MDBDropdown>
+                                    <MDBDropdownToggle className="dopdown-toggle" nav>
+                                        <img src={localStorage.getItem("img_url")} className="rounded-circle z-depth-0"
+                                             style={{ height: "35px", padding: 0 }} alt="" />
+                                    </MDBDropdownToggle>
+                                    <MDBDropdownMenu className="dropdown-default" right>
+                                        <MDBDropdownItem href="/profile">My account</MDBDropdownItem>
+                                        <MDBDropdownItem href="#!" onClick={this.signOut}>Log out</MDBDropdownItem>
+                                    </MDBDropdownMenu>
+                                </MDBDropdown>
+                            ) : (
+                                <MDBNavItem>
+                                    <MDBBtn color="success" onClick={auth0Client.signIn}>
+                                        Login
+                                    </MDBBtn>
+                                </MDBNavItem>
+                            ) }
+
+                        </MDBNavItem>
+                    </MDBNavbarNav>
+                </MDBCollapse>
+            </MDBNavbar>
+
         )
     }
 }
