28011c0671a82dbdc0ae2ea9f57c933512f08a24
Sungyun Won <seanwon8@gmail.com>
2019-02-21 15:06:53 -0800

    tested stripe checkout with test api key and credit card information

--- frontend/shopping-list/src/components/BillingForm.js
+++ frontend/shopping-list/src/components/BillingForm.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import PropTypes from 'prop-types';
 
 class BillingForm extends React.Component {
-    render() {
+    onToken = (res) => {
+        console.log('On Token Called!');
+        console.log(res);
+        console.log(res.id);
+    }
+        
+    render(){
         return(
-           <StripeCheckout
+            <StripeCheckout
                 name="Shoptrak"
                 description="Purchase Subscription"
-           >
-               {this.props.children}
-           </StripeCheckout> 
+                stripeKey="pk_test_YRDXagNKMjZOXlX2ULVNUWbT"
+                currency="USD"
+                token={res => this.onToken(res)}
+            >
+                {this.props.children}
+            </StripeCheckout> 
         )
     }
 }
--- frontend/shopping-list/src/components/BillingPage.js
+++ frontend/shopping-list/src/components/BillingPage.js
@@ -16,22 +16,6 @@ class BillingPage extends Component {
           <Navigation />
           <div className="billing-container">
             <div className="billing">
-              <h1>Billing</h1>
-              <h4>Payment Info</h4>
-              <form className="payment-form">
-                <div className="input-div">
-                  <p>CC#</p>
-                  <input placeholder="####-####-####-####" />
-                </div>
-                <div className="input-div">
-                  <p>EXP</p>
-                  <input placeholder="MM/YY" />
-                </div>
-                <div className="input-div">
-                  <p>CVV</p>
-                  <input placeholder="###" />
-                </div>
-              </form>
               <form>
                 <input 
                   type="radio" 
