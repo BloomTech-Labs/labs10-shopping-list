6f839617582ab1a75d56914207dd74832479c8b7
Nikita Thomas <nikcxz@gmail.com>
2019-02-26 13:34:40 -0500

    appdelegate ctr + i

--- iOS/ShoppingList/ShoppingList/AppDelegate.swift
+++ iOS/ShoppingList/ShoppingList/AppDelegate.swift
@@ -15,26 +15,26 @@ let defaults = UserDefaults.standard
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         window = UIWindow()
         window?.makeKeyAndVisible()
-     let accessToken = A0SimpleKeychain(service: "Auth0").string(forKey:"access_token")
+        let accessToken = A0SimpleKeychain(service: "Auth0").string(forKey:"access_token")
         
- 
-       // let isLoggedIn = defaults.bool(forKey: Keys.isUserLoggedInKey)
+        
+        // let isLoggedIn = defaults.bool(forKey: Keys.isUserLoggedInKey)
         let loginVC = LoginViewController.instantiate()
         let mainVC = MainViewController.instantiate()
         
         
-            window?.rootViewController = (accessToken != nil) ? mainVC : loginVC
-     
+        window?.rootViewController = (accessToken != nil) ? mainVC : loginVC
+        
         return true
     }
-
+    
     //Auth0 requires this function in AppDelegate
     
     func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any]) -> Bool {
