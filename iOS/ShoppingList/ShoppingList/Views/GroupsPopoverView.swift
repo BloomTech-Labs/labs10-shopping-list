0d8194b608428be1fb707ca7a0ae1390c0c398ff
Jason Modisett <jason@Jasons-MacBook-Pro.local>
2019-02-26 12:46:45 -0600

    Fixed profile authentication and loading

---
+++ iOS/ShoppingList/ShoppingList/Views/GroupsPopoverView.swift
@@ -0,0 +1,9 @@
+//
+//  GroupsPopoverView.swift
+//  ShoppingList
+//
+//  Created by Jason Modisett on 2/26/19.
+//  Copyright © 2019 Lambda School Labs. All rights reserved.
+//
+
+import Foundation
