964c41ab7c85a1bc0442fd601cd9d45f0f3ed87b
Nikita Thomas <nikcxz@gmail.com>
2019-02-28 13:19:34 -0500

    HistoryController class

--- iOS/ShoppingList/ShoppingList/View Controllers/MainViewController/MainViewController.swift
+++ iOS/ShoppingList/ShoppingList/View Controllers/MainViewController/MainViewController.swift
@@ -30,6 +30,8 @@ class MainViewController: UIViewController, StoryboardInstantiatable, GroupsPopo
                 }
                 
             }
+            
+            
         }
     }
     
