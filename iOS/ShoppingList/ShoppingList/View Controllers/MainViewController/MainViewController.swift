9733cbb792cad5cccefce2c3ed8849e7c7ca991a
Jason Modisett <jason@Jasons-MacBook-Pro.local>
2019-03-07 08:52:57 -0600

    Created checkout button logic, fixed bugs with swipe to delete, and more

--- iOS/ShoppingList/ShoppingList/View Controllers/MainViewController/MainViewController.swift
+++ iOS/ShoppingList/ShoppingList/View Controllers/MainViewController/MainViewController.swift
@@ -14,10 +14,12 @@ class MainViewController: UIViewController, StoryboardInstantiatable, PopoverVie
     static let storyboardName: StoryboardName = "MainViewController"
     @IBOutlet weak var groupName: UILabel!
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var addNewItemContainer: UIView!
+    @IBOutlet weak var checkoutContainer: UIView!
     
     enum GroupView { case list, history, stats }
     
-    var currentView: GroupView = .list { didSet { updateTableView() }}
+    var currentView: GroupView = .list { didSet { updatesNeeded() }}
     
     // MARK: - Lifecycle methods
     
@@ -54,46 +56,30 @@ class MainViewController: UIViewController, StoryboardInstantiatable, PopoverVie
             currentView = .list
         }
     }
-
+    
     
     
     func updatesNeeded() {
         guard let selectedGroup = selectedGroup else { return }
+        selectedItems = []
+        addNewItemContainer.alpha = 1
+        checkoutContainer.alpha = 0
         groupName.text = selectedGroup.name
-        ItemController.shared.loadItems { (_) in
-            UI { self.tableView.reloadData() }
-        }
-    }
-    
-    func updateTableView() {
+        
         switch currentView {
         case .list:
             tableView.rowHeight = 80
-            
-            DispatchQueue.main.async {
-                self.tableView.reloadData()
+            ItemController.shared.loadItems { (_) in
+                UI { self.tableView.reloadData() }
             }
         case .history:
-            HistoryController().getHistory { (success) in
-                
-                if success {
-                    DispatchQueue.main.async {
-                        self.tableView.reloadData()
-                    }
-                }
-            }
-            
             tableView.rowHeight = 200
-            
+            HistoryController().getHistory { (_) in
+                UI { self.tableView.reloadData() }
+            }
         case .stats:
-            GroupMemberController().getGroupMembers { (success) in
-                if success {
-                    DispatchQueue.main.async {
-                        self.tableView.reloadData()
-                    }
-                } else {
-                    // TODO: Show error getting stats
-                }
+            GroupMemberController().getGroupMembers { (_) in
+                UI { self.tableView.reloadData() }
             }
         }
     }
@@ -133,27 +119,31 @@ extension MainViewController: UITableViewDataSource, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ReuseIdentifier", for: indexPath)
+        cell.tintColor = UIColor(named: "Theme")
         guard let selectedGroup = selectedGroup else { return cell }
         
         var item: Item?
         
         switch currentView {
         case .list:
-            item = selectedGroup.items?[indexPath.row]
+            guard let item = selectedGroup.items?[indexPath.row] else { return cell }
+            cell.accessoryType = selectedItems.contains(item) ? .checkmark : .none
+            cell.textLabel?.text = item.name
         case .history:
             item = history[indexPath.row]
+            cell.textLabel?.text = item?.name
         case .stats:
             let groupMember = groupMembers[indexPath.row]
             cell.textLabel?.text = String(groupMember.net)
         }
         
-        guard item != nil else { return cell }
-        cell.textLabel?.text = item!.name
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
+        guard let cell = tableView.cellForRow(at: indexPath) else { return }
+        cellSelected(cell: cell, indexPath: indexPath)
     }
     
     
@@ -173,13 +163,16 @@ extension MainViewController: UITableViewDataSource, UITableViewDelegate {
     func deleteAction(at indexPath: IndexPath) -> UIContextualAction {
         
         let action = UIContextualAction(style: .destructive, title: "delete") { (action, view, completion) in
-        
             guard let selectedGroup = selectedGroup else { return }
-                 guard let item = selectedGroup.items?[indexPath.row] else { return }
-            ItemController.shared.deleteItem(id: item.id!) {(_) in
-           
-        }
-         completion(true)
+            guard let item = selectedGroup.items?[indexPath.row] else { return }
+            ItemController.shared.deleteItem(id: item.id ?? 0) {(_) in }
+            if selectedItems.contains(item) {
+                let index = selectedItems.index(of: item) ?? 0
+                selectedItems.remove(at: index)
+            }
+            selectedGroup.items?.remove(at: indexPath.row)
+            self.tableView.deleteRows(at: [indexPath], with: .automatic)
+            completion(true)
         }
         action.backgroundColor = .red
         action.title = "Delete"
@@ -187,6 +180,25 @@ extension MainViewController: UITableViewDataSource, UITableViewDelegate {
         return action
     }
     
+    private func cellSelected(cell: UITableViewCell, indexPath: IndexPath) {
+        switch currentView {
+        case .list:
+            guard let item = selectedGroup?.items?[indexPath.row] else { return }
+            if cell.accessoryType == .none {
+                selectedItems.append(item)
+                cell.accessoryType = .checkmark
+            } else {
+                let index = selectedItems.index(of: item) ?? 0
+                selectedItems.remove(at: index)
+                cell.accessoryType = .none
+            }
+        default:
+            break
+        }
+        
+        let showCheckout = selectedItems.count > 0
+        addNewItemContainer.alpha = showCheckout ? 0 : 1
+        checkoutContainer.alpha = showCheckout ? 1 : 0
+    }
     
-
 }
