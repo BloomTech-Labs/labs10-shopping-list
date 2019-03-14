524f1b92999cb9de1ce91f4ef0ab57ade450752e
iAmAdamReid <adam@adamreid.me>
2019-03-14 14:56:35 -0700

    change purchasedOn to string in the history table

--- backend/data/dev/migrations/20190214134738_history_table.js
+++ backend/data/dev/migrations/20190214134738_history_table.js
@@ -6,7 +6,7 @@ exports.up = function(knex, Promise) {
         table.integer('itemID').notNullable();
         table.string('imageURL', 255);
         table.float('total').notNullable();
-        table.date('purchasedOn').notNullable();
+        table.string('purchasedOn').notNullable();
         table.timestamp('createdAt').defaultTo(knex.fn.now());
         table.timestamp('updatedAt').defaultTo(knex.fn.now());
 
